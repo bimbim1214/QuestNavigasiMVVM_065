2c33092d6cb8a4b95238d4ee4312d99491ec33d6
bimbim1214 <bimoadityapangestu@gmail.com>
2024-11-21 19:18:38 +0700

    membuat objek DataJenisKelamin

---
+++ app/src/main/java/com/example/arsitekturmvvm/model/DataJenisKelamin.kt
@@ -0,0 +1,11 @@
+package com.example.arsitekturmvvm.model
+
+import com.example.arsitekturmvvm.R
+
+object DataJenisKelamin {
+        val listJk = listOf(
+            R.string.laki,
+            R.string.wanita,
+        )
+
+}
